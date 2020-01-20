ad17d69b20b0f4faf5150f457876410fe2e78508
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-01-20 15:24:42 -0300

    Configured API and registration feature

--- app.js
+++ app.js
@@ -13,8 +13,41 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   const { first, last, email } = req.body
-  console.log(first, last, email)
-  res.send('done')
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: first,
+          LNAME: last
+        }
+      }
+    ]
+  }
+
+  const jsonData = JSON.stringify(data)
+
+  const options = {
+    url: 'https://us4.api.mailchimp.com/3.0/lists/3204c478e3/',
+    method: 'POST',
+    headers: {
+      'Authorization': 'gusprado f03096a52430875dda79ca75b90715eb-us4'
+    },
+    //body: jsonData
+  }
+
+  request(options, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      res.send('Something went wrong')
+    } else if (response.statusCode === 200) {
+      res.send('You got it')
+    }
+  })
+
 })
 
 app.listen(3000, () => {console.log('Srv running on 3000')})
+
+// key: f03096a52430875dda79ca75b90715eb-us4
+// id: 3204c478e3
